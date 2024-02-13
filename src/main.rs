cd21b382c339fe6900717f797d5a23238dab2aca
Tj Campanella <tj@arete.health>
2024-02-12 23:03:47 -0500

    Add support for compiling more instructions.
    Dump doesn't convert the characters into their proper ascii digit yet.

--- src/main.rs
+++ src/main.rs
@@ -93,17 +93,24 @@ fn compile_program_darwin_arm64(program: Vec<Op>) {
         let mut file = LineWriter::new(file);
         let _ = file.write(b".global _start\n");
         let _ = file.write(b".align 2\n\n");
+        let _ = file.write(b".text\n");
+        let _ = file.write(b"print:\n");
+        let _ = file.write(b"    adrp x0, num@PAGE\n");
+        let _ = file.write(b"    add x0, x0, num@PAGEOFF\n");
+        let _ = file.write(b"    ldr   x1, [sp], #16\n");
+        let _ = file.write(b"    str x1, [x0]\n");
+        let _ = file.write(b"    adrp x1, num@PAGE\n");
+        let _ = file.write(b"    add x1, x1, num@PAGEOFF\n");
+        let _ = file.write(b"    mov	X0, #1\n");
+        let _ = file.write(b"    mov	X2, #3\n");
+        let _ = file.write(b"    mov	X16, #4\n");
+        let _ = file.write(b"    svc	#0x80\n");
+        let _ = file.write(b"    ret\n\n");
         let _ = file.write(b"_start: \n");
-
         for op in program {
             match op.kind {
                 OpKind::Push => {
                     if let Some(val) = op.value {
-                        //mov x0, #34
-                        //str x0, [sp, #-16]!
-                        //mov x0, #35
-                        //str x0, [sp, #-16]!
-
                         let _ = file.write(b"    // push \n");
                         let _ = file.write(format!("    mov x0, #{val}\n").as_bytes());
                         let _ = file.write("    str x0, [sp, #-16]!\n".to_string().as_bytes());
@@ -111,12 +118,15 @@ fn compile_program_darwin_arm64(program: Vec<Op>) {
                 }
                 OpKind::Plus => {
                     let _ = file.write(b"    // plus \n");
-                    let _ = file.write("    ldr   x0, [sp], #16\n".to_string().as_bytes());
-                    let _ = file.write("    ldr   x1, [sp], #16\n".to_string().as_bytes());
-                    let _ = file.write("    add   x3, x0, x1\n".to_string().as_bytes());
-                    let _ = file.write("    str x3, [sp, #-16]!\n".to_string().as_bytes());
+                    let _ = file.write(b"    ldr   x0, [sp], #16\n");
+                    let _ = file.write(b"    ldr   x1, [sp], #16\n");
+                    let _ = file.write(b"    add   x3, x0, x1\n");
+                    let _ = file.write(b"    str x3, [sp, #-16]!\n");
                 }
-                OpKind::Dump => {}
+                OpKind::Dump => {
+                    let _ = file.write(b"    // dump \n");
+                    let _ = file.write(b"    bl print\n");
+                }
             }
             let _ = file.write(b"\n");
         }
@@ -125,6 +135,9 @@ fn compile_program_darwin_arm64(program: Vec<Op>) {
         let _ = file.write(b"    mov x0, #0\n");
         let _ = file.write(b"    mov x16, #1\n");
         let _ = file.write(b"    svc #0x80\n");
+
+        let _ = file.write(b".data\n");
+        let _ = file.write(b"    num: .zero 8\n");
     }
 }
 
